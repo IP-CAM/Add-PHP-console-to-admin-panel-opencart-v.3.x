1df52f71a17eae0395e79bfec35c245f9945a6c7
Ivanov A <entense@yandex.ru>
2021-01-14 01:58:37 +0300

    Create console.php

---
+++ admin/language/en-gb/tool/console.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']     = 'PHP console';
+
+// Text
+$_['text_php']     = 'PHP code';
+$_['text_show_report']     = 'Show report';
+$_['text_show_errors']     = 'Show errors';
+
+
+// Button
+$_['button_execute']     = 'Execute';
